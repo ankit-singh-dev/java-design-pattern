0c76e1a34d13c241b7aa7ad4275fcf2b31630c77
Ankit Singh <ankit_singh1@epam.com>
2022-08-14 17:10:52 +0530

    adapter design pattern tutorials

---
+++ src/main/java/org/example/structuraldesignpattern/AdapterDesignPattern.java
@@ -0,0 +1,97 @@
+package org.example.structuraldesignpattern;
+
+/*
+Adapter design pattern is one of the structural design pattern and its used so that two unrelated
+interfaces can work together. The object, that joins these unrelated interfaces, is called an Adapter.
+ */
+
+/*
+https://www.baeldung.com/java-adapter-pattern
+https://www.tutorialspoint.com/design_pattern/adapter_pattern.htm#
+https://www.geeksforgeeks.org/adapter-pattern/
+ */
+
+/*
+generate korean movies subtitle using adapter design pattern
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// korean movies player interface
+interface KoreanMediaPlayer{
+    void playKoreanMovies(String fileName);
+}
+
+// korean movies player implementation
+class KoreanMediaPlayerImpl implements KoreanMediaPlayer{
+
+    @Override
+    public void playKoreanMovies(String fileName) {
+        System.out.println("Playing korean movie "+fileName);
+    }
+}
+
+// english movies player interface
+interface EnglishMediaPlayer{
+    void playEnglishMedia(String fileName);
+}
+
+// english movies player implementation
+class EnglishMediaPlayerImpl implements EnglishMediaPlayer{
+
+    @Override
+    public void playEnglishMedia(String fileName) {
+        System.out.println("Playing English movie "+fileName);
+    }
+}
+
+// Generates english subtitle for koran movies
+class AdapterClass implements KoreanMediaPlayer{
+
+    private final KoreanMediaPlayer koreanMediaPlayer;
+    private Map<String,String> subtitleMap;
+
+    public AdapterClass(KoreanMediaPlayer koreanMediaPlayer) {
+        this.koreanMediaPlayer = koreanMediaPlayer;
+        initializeSubtitleMap();
+    }
+
+    private void initializeSubtitleMap(){
+        subtitleMap = new HashMap<>();
+        subtitleMap.put("안녕하세요","Hello");
+        subtitleMap.put("안녕히 계세요","Bye");
+    }
+
+    private List<String> convertKoreanMoviesToEnglish(String fileName){
+        // some logic to generate korean movie subtitle
+        List<String> subtitles = new ArrayList<>();
+        for(String str : subtitleMap.values()){
+            subtitles.add(str);
+        }
+        return subtitles;
+    }
+
+    @Override
+    public void playKoreanMovies(String fileName) {
+        List<String> subtitles = convertKoreanMoviesToEnglish(fileName);
+        System.out.println("Playing korean movie "+fileName+ " with english subtitles");
+        System.out.println("Subtitles are");
+        subtitles.forEach(System.out::println);
+    }
+}
+
+public class AdapterDesignPattern {
+    public static void main(String[] args) {
+        KoreanMediaPlayer koreanMediaPlayer = new KoreanMediaPlayerImpl();
+        koreanMediaPlayer.playKoreanMovies("train to busan");
+
+        EnglishMediaPlayer englishMediaPlayer = new EnglishMediaPlayerImpl();
+        englishMediaPlayer.playEnglishMedia("inception");
+
+        KoreanMediaPlayer koreanMediaPlayerAdapter = new AdapterClass(koreanMediaPlayer);
+        koreanMediaPlayerAdapter.playKoreanMovies("train to busan");
+    }
+}
